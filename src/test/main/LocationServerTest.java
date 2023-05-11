09a6ebb20b13eda36ffcc950c6983239ab04f10c
anhminhbo <wormscott12397@gmail.com>
2023-05-11 17:32:25 +0700

    chore: init

---
+++ src/test/main/LocationServerTest.java
@@ -0,0 +1,12 @@
+package main;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LocationServerTest {
+    @Test
+    public void demoTestMethod() {
+        assertTrue(true);
+    }
+}
