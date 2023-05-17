897aee573f5d17cda0cd1bdaa0500b8b3a065bb2
Kendrick2000 <56669029+Kendrick2000@users.noreply.github.com>
2023-05-17 16:41:59 +0700

    edit printMessage

--- src/test/EnvironAPPUI/PrintMessageTest.java
+++ src/test/EnvironAPPUI/PrintMessageTest.java
@@ -7,7 +7,7 @@ public class PrintMessageTest {
 
     @Test
     public void testPrintMessage(){
-        String[] str = {"", "Hello World", "\t"};
+        String[] str = {"", "Hello World", "\tHelloWorld", "123", "@##$@!$@!"};
         for (int i = 0; i < str.length; i++){
             EnviroAPPUI.printMessage(str[i]);
         }
